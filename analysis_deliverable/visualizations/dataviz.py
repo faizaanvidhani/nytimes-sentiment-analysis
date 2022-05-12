a2841884fd0c06765bc3fdad613ba4fbb0bb0682
Faizaan Vidhani <faizaanvidhani@gmail.com>
2022-05-12 01:20:55 -0400

    added pie charts to final deliverable folder

--- analysis_deliverable/visualizations/dataviz.py
+++ analysis_deliverable/visualizations/dataviz.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from collections import defaultdict
 
 
 #global variable name for column
@@ -117,10 +118,92 @@ def plot_author_line():
     '''
     pass
 
+def get_value_counts_table(df, col_name):
+    # get counts
+    count_dict = defaultdict(int)
+    def update_count_dict(r):
+        count_dict[r[col_name]] += 1
+    df.apply(lambda x: update_count_dict(x), axis=1)
+    rows = [[e, count_dict[e]] for e in count_dict]
+    count_df = pd.DataFrame(rows, columns=[col_name, "count"])
+    x = count_df.sort_values(by=['headline_ratings'])
+
+    # combine 0 and 1 into 'Negative'
+    df_negs = x.copy()
+    df_negs.loc[df_negs['headline_ratings'] < 2, 'headline_ratings'] = 1
+    df_negs = df_negs.groupby('headline_ratings')['count'].sum().reset_index()
+
+    # combine 3 and 4 into 'Positive'
+    df_pos = df_negs.copy()
+    df_pos.loc[df_pos['headline_ratings'] > 2, 'headline_ratings'] = 3
+    df_pos = df_pos.groupby('headline_ratings')['count'].sum().reset_index()
+
+
+    return df_pos
+
+
+def art_pie_chart(data):
+    # # TODO: Create a figure with just one subplot - hint: the link above
+    my_fig, my_ax = plt.subplots(1)
+
+    # TODO: Redefine somewhat_better_plot to satisfy the aforementioned requirements
+    # Note: The amount of space should not represent how many things you should pass
+    # into the .plot function call!
+    somewhat_better_plot = data["count"].plot(kind='pie',
+                                                            autopct='%1.1f%%',
+                                                            pctdistance=1.2,
+                                                            labeldistance=1.2,
+                                                            startangle=90,
+                                                            title = "Proportion of Arts Headlines by Sentiment Rating",
+                                                            ax=my_ax,
+                                                            colormap="RdYlGn",
+                                                            labels=["" for e in data["headline_ratings"]])
+
+
+
+    # TODO: Pass in the review_count_table["review_reason_code"] as the label to draw
+    # the legend on the Axes my_ax
+    my_ax.set_ylabel("")
+    plt.legend(['Negative', 'Positive'], bbox_to_anchor=(.9, .85))
+    plt.show()
+
+def tech_pie_chart(data):
+    # # TODO: Create a figure with just one subplot - hint: the link above
+    my_fig, my_ax = plt.subplots(1)
+
+    # TODO: Redefine somewhat_better_plot to satisfy the aforementioned requirements
+    # Note: The amount of space should not represent how many things you should pass
+    # into the .plot function call!
+    somewhat_better_plot = data["count"].plot(kind='pie',
+                                                            autopct='%1.1f%%',
+                                                            pctdistance=1.2,
+                                                            labeldistance=10000,
+                                                            startangle=90,
+                                                            title = "Proportion of Technology Headlines by Sentiment Ratings",
+                                                            ax=my_ax,
+                                                            colormap="RdYlGn",
+                                                            labels=["" for e in data["headline_ratings"]])
+
+
+
+    # TODO: Pass in the review_count_table["review_reason_code"] as the label to draw
+    # the legend on the Axes my_ax
+    my_ax.set_ylabel("")
+    plt.legend(['Negative', 'Neutral', 'Positive'], bbox_to_anchor=(.9, .85))
+    plt.show()
+
   
   
 if __name__ == "__main__":
     data = df = pd.read_csv("../updated_nyt_data.csv")
     # sentiment_score_histogram(data)
-    h2_box_plot(data)
-    plot_category_bar('Arts', 'Technology', data)
+    # h2_box_plot(data)
+    # plot_category_bar('Arts', 'Technology', data)
+    arts = data[data['category'] == 'Arts']
+    tech = data[data['category'] == 'Technology']
+    arts_headline_counts = get_value_counts_table(arts, "headline_ratings")
+    tech_headline_counts = get_value_counts_table(tech, "headline_ratings")
+    art_pie_chart(arts_headline_counts)
+    tech_pie_chart(tech_headline_counts)
+    print(arts_headline_counts)
+    print(tech_headline_counts)
